5e9aa91becf82cfd76dc95941bbf175770faaa84
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-03-14 16:00:04 +0100

    flex-col on mobile

--- config/menu.php
+++ config/menu.php
@@ -3,12 +3,12 @@
 return [
     'classes' => [
         1 => [
-            'ul'       => 'flex flex-wrap',
+            'ul'       => 'flex flex-wrap max-sm:flex-col',
             'li'       => 'group',
             'category' => 'block p-3 text-primary font-bold',
         ],
         2 => [
-            'ul'       => 'hidden absolute flex bg-white border p-3 z-10 group-hover:flex',
+            'ul'       => 'hidden absolute flex bg-white border p-3 z-10 sm:group-hover:flex',
             'li'       => 'px-3',
             'category' => 'block text-primary font-bold py-3',
         ],

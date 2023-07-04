372f6eb4d2028e49503e0e658f4bf22c5d89b0e1
indy koning <indy@justbetter.nl>
2023-07-04 10:02:02 +0200

    remove to helper

--- resources/views/menu/item.blade.php
+++ resources/views/menu/item.blade.php
@@ -1,5 +1,5 @@
 <li class="{{ config('menu.classes.'.$loop->depth.'.li') }}">
-    <a class="{{ config('menu.classes.'.$loop->depth.'.category') }}" href="{{ to($item->url) }}">
+    <a class="{{ config('menu.classes.'.$loop->depth.'.category') }}" href="{{ url($item->url) }}">
         {{ $item->name }}
     </a>
     @includeWhen($item->children->count(), 'rapidez::menu.menu', ['items' => $item->children])

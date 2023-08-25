4ac101216779f86499b982c55d84b944f61463e9
Jimmy <101801301+JimmyHoenderdaal@users.noreply.github.com>
2023-08-25 08:19:21 +0200

    Changed the default colors (#6)

--- config/menu.php
+++ config/menu.php
@@ -5,16 +5,16 @@ return [
         1 => [
             'ul'       => 'flex flex-wrap max-sm:flex-col',
             'li'       => 'group',
-            'category' => 'block p-3 text-primary font-bold',
+            'category' => 'block p-3 text-neutral font-bold',
         ],
         2 => [
             'ul'       => 'hidden absolute flex bg-white border p-3 z-10 sm:group-hover:flex',
             'li'       => 'px-3',
-            'category' => 'block text-primary font-bold py-3',
+            'category' => 'block text-neutral font-bold py-3',
         ],
         3 => [
             'ul'       => 'text-sm',
-            'category' => 'inline-block py-1 hover:text-primary',
+            'category' => 'inline-block py-1 hover:text-neutral',
         ],
     ],
 ];

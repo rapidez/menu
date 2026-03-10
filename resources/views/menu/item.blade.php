c8f309bcf389da80e390deec3f1209ac998f9e76
indykoning <15870933+indykoning@users.noreply.github.com>
2026-03-10 11:50:35 +0100

    Unescape category param

--- resources/views/menu/item.blade.php
+++ resources/views/menu/item.blade.php
@@ -1,6 +1,6 @@
 <li class="{{ config('rapidez.menu.classes.'.$loop->depth.'.li') }}">
     <a class="{{ config('rapidez.menu.classes.'.$loop->depth.'.category') }}" href="{{ url($item->url) }}">
-        {{ $item->name }}
+        {!! $item->name !!}
     </a>
     @includeWhen($item->children->count(), 'rapidez::menu.menu', ['items' => $item->children])
 </li>
